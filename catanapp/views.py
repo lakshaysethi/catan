665e0e42b61bf885dee71c18e1fa78fa2fdaf099
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-10-05 01:33:02 +1300

    Update views.py

--- catanapp/views.py
+++ catanapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from catanapp.models import Dropable
 # Create your views here.
-
+from Catan.settings import BASE_DIR
+from django.core.files.images import ImageFile
 
 def home(request):
     ctx = {}
@@ -11,4 +12,15 @@ def home(request):
 
 def play(request):
     ctx = {}
+                                                                 
+        
+    # card1 = Dropable.objects.create(name='card1',image = f'{BASE_DIR}/static/dropables/rockhex.png') 
+    
+    # card1.save()
+
+    imageObjects = Dropable.objects.all()
+    imageObjectUrls = [a.image.url.split('static')[1] for a in imageObjects]
+
+    ctx['imageObjects'] = imageObjects
+    ctx['imageObjectUrls'] = imageObjectUrls
     return render (request,'play.html',ctx)
