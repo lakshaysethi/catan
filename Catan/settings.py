a154cb2c15ae1bddddef46607faa52cf4a61d1ce
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-10-05 01:33:12 +1300

    updates

--- Catan/settings.py
+++ Catan/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_werkzeug',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catanapp',
+    
 ]
 
 MIDDLEWARE = [
@@ -119,7 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- Catan/urls.py
+++ Catan/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from catanapp import views 
 
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('play/', views.play,name = 'play'),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
