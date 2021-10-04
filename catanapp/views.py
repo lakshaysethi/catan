bcb1571339f38484d839a243477788bfe0f4971b
lakshaysethi <45325278+lakshaysethi@users.noreply.github.com>
2021-10-05 00:02:12 +1300

    bade basic game structure

--- catanapp/views.py
+++ catanapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    ctx = {}
+    return render (request,'home.html',ctx)
+
+
+
+def play(request):
+    ctx = {}
+    return render (request,'play.html',ctx)
